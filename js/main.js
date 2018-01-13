e3face5a8f54320d9c188d9807a794e9b0bc6211
mozukraken <mozukraken@gmail.com>
2018-01-13 10:23:53 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+(function() {
+  'use strict';
+
+  var timer = document.getElementById('timer');
+  var start = document.getElementById('start');
+  var stop = document.getElementById('stop');
+  var reset = document.getElementById('reset');
+
+  var startTime;
+  var elapsedTime = 0;
+
+  function updateTimerText() {
+    var m = Math.floor(elapsedTime / 60000);
+    var s = Math.floor(elapsedTime % 60000 / 1000);
+    var ms = elapsedTime % 1000;
+    timer.textContent = m + ":" + s + "." + ms;
+  }
+
+  function countUp() {
+    setTimeout(function() {
+      elapsedTime = Date.now() - startTime;
+      countUp();
+      updateTimerText();
+    }, 10);
+  }
+  start.addEventListener('click', function() {
+    startTime = Date.now();
+    countUp();
+  });
+
+})();
