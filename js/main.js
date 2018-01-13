b6fc971cbe4d3729d2fa548ba7f8c036932c495c
mozukraken <mozukraken@gmail.com>
2018-01-13 22:43:35 +0900

    ストップウォッチの機能完成

--- js/main.js
+++ js/main.js
@@ -8,24 +8,54 @@
 
   var startTime;
   var elapsedTime = 0;
+  var timerId;
+  var timeToAdd = 0;
+  var isRunning = false;
 
   function updateTimerText() {
     var m = Math.floor(elapsedTime / 60000);
     var s = Math.floor(elapsedTime % 60000 / 1000);
     var ms = elapsedTime % 1000;
+
+    m = ('0' + m).slice(-2);
+    s = ('0' + s).slice(-2);
+    ms = ('00' + ms).slice(-3);
+
     timer.textContent = m + ":" + s + "." + ms;
   }
 
   function countUp() {
-    setTimeout(function() {
-      elapsedTime = Date.now() - startTime;
+    timerId = setTimeout(function() {
+      elapsedTime = Date.now() - startTime + timeToAdd;
       countUp();
       updateTimerText();
     }, 10);
   }
   start.addEventListener('click', function() {
+    if (isRunning === true) {
+      return;
+    }
+    isRunning = true;
     startTime = Date.now();
     countUp();
+  });
+
+  stop.addEventListener('click', function() {
+    if (isRunning === false) {
+      return;
+    }
+    isRunning = false;
+    clearTimeout(timerId);
+    timeToAdd += Date.now() - startTime;
+  });
+
+  reset.addEventListener('click', function() {
+    if (isRunning === true) {
+      return;
+    }
+    elapsedTime = 0;
+    timeToAdd = 0;
+    updateTimerText();
   });
 
 })();
